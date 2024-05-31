eaebbd33ccfcf25c12c22ef2f25ebcadafda8d8f
= <=>
2024-06-01 00:30:04 +0300

    week4-loops

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while(loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+
+    Console.WriteLine("Make a guess. Enter a number from 1 to 3");
+    
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    } else
+    {
+        Console.WriteLine("You lost, try again");
+    }
+}
